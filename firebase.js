c5a1d037359947fc67b292346b4ead963b5ffe96
himanshupalsingh6-dev <himanshupalsingh6@gmail.com>
2026-01-02 17:56:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-// Your Firebase configuration (JO AAPNE DIYA)
+// 🔴 Firebase config (JO AAPNE DIYA)
 const firebaseConfig = {
-  apiKey: "AIzaSyCo5FqqmJvu0LAaDSXuMv_jafo1ZbLw18",
+  apiKey: "AIzaSyCo5FqqmJvu0LAaDSXuMv_jafo1ZbLwW18",
   authDomain: "quickpress-24422.firebaseapp.com",
   projectId: "quickpress-24422",
-  storageBucket: "quickpress-24422.firebasestorage.app",
+  storageBucket: "quickpress-24422.appspot.com",
   messagingSenderId: "602677592052",
   appId: "1:602677592052:web:b17f44c69b6283fc6843a1"
 };
@@ -18,6 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services (VERY IMPORTANT)
+// ✅ EXPORT (bahut zaroori)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
