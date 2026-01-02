b40b0488471c10ae4a19bcc2e7eb864d0994886d
himanshupalsingh6-dev <himanshupalsingh6@gmail.com>
2026-01-02 17:13:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCo5FqqmJvu0LAaDSXuMv_jafo1ZbLwW18",
+    authDomain: "quickpress-24422.firebaseapp.com",
+    projectId: "quickpress-24422",
+    storageBucket: "quickpress-24422.firebasestorage.app",
+    messagingSenderId: "602677592052",
+    appId: "1:602677592052:web:b17f44c69b6283fc6843a1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
